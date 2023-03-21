3e5aad1d21c9612f3d0a90d05a0709d7f54ad28a
Het <desaihetp@gmail.com>
2023-03-21 15:40:30 +0530

    OHM Configured
    OHM and other tools installed

--- zerotoolsscript.sh
+++ zerotoolsscript.sh
@@ -1,81 +1,104 @@
 #!/bin/bash
 
 # System package update and upgrade
-apt-get -y update && apt-get -y upgrade && apt-get -y full-upgrade
+sudo apt-get -y update && sudo apt-get -y upgrade
+
+echo "----------------------------------------------------------------"
+
+# Remove packages which are unwanted
+sudo apt-get auto-remove
 
 echo "----------------------------------------------------------------"
 
 # Install softwares
-apt-get install -y git curl wget rename xargs
+sudo apt-get install -y git curl wget rename xargs
 
-apt-get install -y python3 python2 python-pip python-dnspython python-setuptools python-dev
+# Python programming language and their packages
+sudo apt-get install -y python3 python2 python-pip python-dnspython python-setuptools python-dev
 
-apt-get install -y nmap
+# network scanning tool
+sudo apt-get install -y nmap
 
-# apt-get install -y jq
+# lightweight and flexible command-line JSON processor
+sudo apt-get install -y jq
 
-# apt-get install ruby-full
+# Programming language
+sudo apt-get install -y ruby-full ruby-dev
 
-# apt-get install -y libcurl4-openssl-dev
+# development files and documentation for libcurl (OpenSSL flavour)
+sudo apt-get install -y libcurl4-openssl-dev
 
-# apt-get install -y libssl-dev
+# Secure Sockets Layer toolkit - development files
+sudo apt-get install -y libssl-dev
 
-# apt-get install -y libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+# sudo apt-get install -y libxml2 libxml2-dev libxslt1-dev build-essential libgmp-dev zlib1g-dev
 
-# apt-get install -y build-essential libssl-dev libffi-dev
+# sudo apt-get install -y build-essential libssl-dev libffi-dev
 
-# apt-get install libldns-dev
+# sudo apt-get install -y libldns-dev
 
 echo "----------------------------------------------------------------"
 
-
+# Download and Install Go
 
 echo "----------------------------------------------------------------"
 
 # Install sublime text
-# cd ~/Downloads/
 
-# echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+cd ~/Downloads/
 
-# echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 
-# apt-get update && apt-get install sublime-text
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
-echo "----------------------------------------------------------------"
-
-# Install firefox
-# cd ~/Downloads/
-
-# curl -L https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US -o  firefox.tar.bz2 && tar -xvf firefox.tar.bz2
+sudo apt-get update && sudo apt-get install sublime-text
 
 echo "----------------------------------------------------------------"
 
 # Install Brave Browser
-# cd ~/Downloads/
 
-# curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+cd ~/Downloads/
 
-# echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && \
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list && \
+sudo apt-get update && sudo apt-get install brave-browser
 
-# apt-get update && apt-get install brave-browser
+echo "----------------------------------------------------------------"
+
+mkdir ~/Tools
 
 echo "----------------------------------------------------------------"
 
 # Download SecList
-cd /opt/
+
+cd ~/Tools
 git clone https://github.com/aboul3la/Sublist3r.git
 git clone https://github.com/danielmiessler/SecLists.git
 
 echo "----------------------------------------------------------------"
 
-# Install OhMyBash
+# OhMyBash installation and setup
+
+cd ~
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
-# mv ~/.bashrc ~/.bashrc.OMB`date "+%H%M%S%d%m%Y"`.bak
+# Take a backup of .bashrc file with time stamp
+mv ~/.bashrc ~/.bashrc.OMB`date "+%H%M%S%d%m%Y"`.bak
 
-# if [[ -f ~/.bashrc ]]; then
-#        source ~/.bashrc
-# fi
+# Take a backup of the .bash_profile file with time stamp and append the three command lines in that
+if [[ -f ~/.bash_profile ]]; then
+cp ~/.bash_profile ~/.bash_profile.OMB`date "+%H%M%S%d%m%Y"`.bak
+tee -a ~/.bash_profile <<EOF
+if [[ -f ~/.bashrc ]]; then
+	source ~/.bashrc
+fi
+EOF
+fi
+
+# Change the theme random to robbyrussell in bash .bashrc file
+sed -i -e 's/OSH_THEME="random"/OSH_THEME="robbyrussell"/g' .bashrc
+
+echo "----------------------------------------------------------------"
 
 # read -p "Press any key to exit the program."
